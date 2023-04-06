dc887fd04c809a2aac3c5205a3fa6f273181ff88
faranbutt <faranbutt789@gmail.com>
2023-04-06 17:37:25 +0500

    Pushing app

--- routes/api.js
+++ routes/api.js
@@ -1,70 +1,50 @@
 const express = require("express");
 const multer = require("multer");
-const axios = require("axios");
-const FormData = require("form-data");
-const path = require("path");
-const { Readable } = require('stream');
-const OpenAIApi = process.env.OPENAI_API_KEY;
+const { Configuration, OpenAIApi } = require("openai");
+
 
 const router = express.Router();
-const upload = multer({
-  fileFilter: function(req, file, cb) {
-    if (!file.originalname.match(/\.(mp3)$/)) {
-      return cb(new Error("Only mp3 files are allowed!"));
-    }
-    cb(null, true);
-  }
+const upload = multer();
+
+const configuration = new Configuration({
+    apiKey: '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',
 });
+
+async function transcribe(buffer) {
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createTranscription(
+        buffer, // The audio file to transcribe.
+        "whisper-1", // The model to use for transcription.
+        undefined, // The prompt to use for transcription.
+        'json', // The format of the transcription.
+        1, // Temperature
+        'en' // Language
+    )
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Correct the grammar in ${response.data.text}`,
+      });
+    return completion;
+}
 
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
-async function transcribe(buffer) {
-    const formData = new FormData();
-    const readable = Readable.from(buffer);
-    formData.append('audio', readable);
-    formData.append('model', 'whisper-1'); // Add the model parameter to formData
-  
-    const response = await axios.post(
-      'https://api.openai.com/v1/audio/transcriptions',
-      formData,
-      {
-        headers: {
-          Authorization:`Bearer ${OpenAIApi}`,
-        "Content-Type":`multipart/form-data; boundary=${formData._boundary}`,
-        "Organization":"org-lba2fQjNS1kIzEITjc8dwFEs"
-
-        },
-        params: {
-          language: 'en',
-          format: 'json',
-          temperature: 1,
-        }
-      }
-    );
-    
-    return response.data;
-  }
-  
 router.post("/", upload.any('file'), (req, res) => {
-    const audio_file = req.files[0];
-    const buffer = audio_file.buffer;
+    audio_file = req.files[0];
+    buffer = audio_file.buffer;
     buffer.name = audio_file.originalname;
-    
-    transcribe(buffer)
-        .then(data => {
-            console.log(data);
-            res.send({ 
-                type: "POST", 
-                transcription: data.text,
-                audioFileName: buffer.name
-            });
+    const response = transcribe(buffer);
+    response.then((data) => {
+        // const text = data.data.text;
+        res.send({ 
+            type: "POST", 
+            transcription: data.data.choices[0].text,
+            audioFileName: buffer.name
         })
-        .catch(error => {
-            console.log(error.response.data);
-            res.send({ type: "POST", message: error.message });
-        });
+    })
 });
 
 module.exports = router;
+
